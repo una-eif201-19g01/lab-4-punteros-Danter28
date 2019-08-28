09a58e07b9a4aaa84fae8f9c5846549b7ceabf5f
Danter28 <danter28@gmail.com>
2019-08-28 11:01:46 -0600

    añadi documentacion

--- Empleado.h
+++ Empleado.h
@@ -30,14 +30,49 @@ protected:
 	bool revision;
 private:
 	
-	
+	/**
+	 *calcula el aumento de salario si el empleado es seleccionado para revision
+	 *
+	 * @param puntero a Empleado.
+	 * @return cantidad de aumento en valor decimal.
+	 */
 	float aumentoSalario(Empleado*);
+	/**
+	 * cambia el valor de revision dependiendo del numero que le entre
+	 *
+	 * @param puntero a Empleado y un entero entre 0 y 1 por referencia
+	 * @return _.
+	 */
 	void revisionAleatoria(Empleado*,int&);
 	
 public:
+	/**
+	 * da forma a la informacion del Empleado para luego mostrarse en pantalla
+	 *
+	 * @param Empleado y un entero por referencia
+	 * @return informacion del Empleado con el formato deseado en forma de string.
+	 */
 	string obtenerReporteEmpleado(Empleado&,int& );
+
+	/**
+	* constructor sin parametros
+	*
+	*/
 	Empleado();
+	/**
+	* constructor con parametros
+	*
+	* @param string para el nombre,entero para los annios de exp y float para salarioBase.
+	*/
 	Empleado(string,int,float);
+
+	//////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * metodos Set y Get
+	 *
+	 * @param los Sets reciben por referencia un valor acorde a lo que se quiere cambiar.
+	 * @return los Gets regresan informacion acorde a lo que se solicita.
+	 */
 	void setNombre(string&);
 	string getNombre();
 	void setAnnios(int&);
